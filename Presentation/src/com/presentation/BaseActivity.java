d67f847d20713bdfc22a4ad22ad5f7ceba6ce978
raj <raj@localhost>
2013-11-23 19:20:56 -0500

    google services commit

--- Presentation/src/com/presentation/BaseActivity.java
+++ Presentation/src/com/presentation/BaseActivity.java
@@ -1,22 +1,157 @@
 package com.presentation;
 
-import android.os.Bundle;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+import android.accounts.AccountManager;
 import android.app.Activity;
-import android.view.Menu;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
+import com.google.api.client.googleapis.extensions.android.gms.auth.UserRecoverableAuthIOException;
+import com.google.api.client.http.FileContent;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.DriveScopes;
+import com.google.api.services.drive.model.File;
+import com.google.api.services.drive.model.FileList;
 
 public class BaseActivity extends Activity {
+	static final int REQUEST_ACCOUNT_PICKER = 1;
+	static final int REQUEST_AUTHORIZATION = 2;
+	static final int CAPTURE_IMAGE = 3;
+	protected static final String TAG = BaseActivity.class.getSimpleName();
+
+	private static Uri fileUri;
+	private static Drive driveService;
+	private GoogleAccountCredential credential;
+	final private List<String> SCOPES = Arrays.asList(new String[]{
+			DriveScopes.DRIVE, DriveScopes.DRIVE_FILE 
+		  });
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.layout_base);
+
+		credential = GoogleAccountCredential.usingOAuth2(this, SCOPES);
+		startActivityForResult(credential.newChooseAccountIntent(), REQUEST_ACCOUNT_PICKER);
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.base, menu);
-		return true;
+	protected void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
+		switch (requestCode) {
+		case REQUEST_ACCOUNT_PICKER:
+			if (resultCode == RESULT_OK && data != null && data.getExtras() != null) {
+				String accountName = data.getStringExtra(AccountManager.KEY_ACCOUNT_NAME);
+				if (accountName != null) {
+					credential.setSelectedAccountName(accountName);
+					driveService = getDriveService(credential);
+//					startCameraIntent();
+					
+					new AsyncTask<Void, Void, Boolean>() {
+
+						FileList driveFileList;
+						
+						@Override
+						protected Boolean doInBackground(Void... params) {
+							try {
+								driveFileList = driveService.files().list().execute();
+							} catch (IOException e) {
+								e.printStackTrace();
+								return false;
+							}
+							return true;
+						}
+						
+						protected void onPostExecute(Boolean result) {
+							for (File temp :  driveFileList.getItems()) {
+								Log.d(TAG, "Filelist : " + temp.getTitle() + " mimetype : " + temp.getMimeType());
+							}
+						}
+					}.execute();
+					
+					
+				}
+			}
+			break;
+		case REQUEST_AUTHORIZATION:
+			if (resultCode == Activity.RESULT_OK) {
+				saveFileToDrive();
+			} else {
+				startActivityForResult(credential.newChooseAccountIntent(), REQUEST_ACCOUNT_PICKER);
+			}
+			break;
+		case CAPTURE_IMAGE:
+			if (resultCode == Activity.RESULT_OK) {
+				saveFileToDrive();
+			}
+		}
 	}
 
+	private void startCameraIntent() {
+		String mediaStorageDir = Environment.getExternalStoragePublicDirectory(
+				Environment.DIRECTORY_PICTURES).getPath();
+		String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(new Date());
+		fileUri = Uri.fromFile(new java.io.File(mediaStorageDir + java.io.File.separator + "IMG_"
+				+ timeStamp + ".jpg"));
+
+		Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+		cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+		startActivityForResult(cameraIntent, CAPTURE_IMAGE);
+	}
+
+	private void saveFileToDrive() {
+		Thread t = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					// File's binary content
+					java.io.File fileContent = new java.io.File(fileUri.getPath());
+					FileContent mediaContent = new FileContent("image/jpeg", fileContent);
+
+					// File's metadata.
+					File body = new File();
+					body.setTitle(fileContent.getName());
+					body.setMimeType("image/jpeg");
+
+					File file = driveService.files().insert(body, mediaContent).execute();
+					if (file != null) {
+						showToast("Photo uploaded: " + file.getTitle());
+						startCameraIntent();
+					}
+				} catch (UserRecoverableAuthIOException e) {
+					startActivityForResult(e.getIntent(), REQUEST_AUTHORIZATION);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		t.start();
+	}
+
+	private Drive getDriveService(GoogleAccountCredential credential) {
+		return new Drive.Builder(AndroidHttp.newCompatibleTransport(), new GsonFactory(),
+				credential).build();
+	}
+
+	public void showToast(final String toast) {
+		runOnUiThread(new Runnable() {
+			@Override
+			public void run() {
+				Toast.makeText(getApplicationContext(), toast, Toast.LENGTH_SHORT).show();
+			}
+		});
+	}
 }
